0630d259c37edf11616c2863c46df4c95d3a688e
neilmrtngamboa <nmg.gamboa@gmail.com>
2024-02-22 11:47:27 +0800

    2nd test

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
     
     
 document.querySelector('#find').addEventListener('click', () => {
-    
+
     const pokemonName = document.querySelector('#search').value.toLowerCase();  
         fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
         .then((response) => response.json())
-        .then((result) => {console.log(result)
+        .then((result) => { //console.log(result.name)
+
+            document.querySelector('#pokemonContent').innerHTML +=
+
+            `The pokemon name is ${result.name}`
 
         })
         .catch((error) => console.error(error));
