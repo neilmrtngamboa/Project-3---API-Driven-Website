7f70508629b1639c109e75bd1f64a2b5940c9c22
neilmrtngamboa <nmg.gamboa@gmail.com>
2024-02-22 11:42:44 +0800

    tested the button and the fetched data

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
+    
+    
+document.querySelector('#find').addEventListener('click', () => {
+    
+    const pokemonName = document.querySelector('#search').value.toLowerCase();  
+        fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
+        .then((response) => response.json())
+        .then((result) => {console.log(result)
 
-document.querySelector('#')
+        })
+        .catch((error) => console.error(error));
+
+})    
+    
